49012b883e4f372bf63364e79491a6712726f790
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-09 00:42:23 +0700

    added useLastExistingChildren hook

---
+++ hooks/lastExistingChildren.ts
@@ -0,0 +1,41 @@
+// react:
+import {
+    // react:
+    default as React,
+    
+    
+    
+    // hooks:
+    useRef,
+}                           from 'react'
+
+
+
+// utilities:
+let childrenKeyCounter = 0;
+
+
+
+// hooks:
+export const useLastExistingChildren = (children?: React.ReactNode): readonly [boolean, React.ReactNode] => {
+    // states:
+    const lastExistingChildrenRef = useRef<React.ReactNode>(undefined);
+    const hasChildren = (!!children || (children === 0)) && (children !== true) && (!Array.isArray(children) || !!children.length); // ignores undefined|null|true|false|emptyString|emptyArray
+    if (hasChildren) {
+        lastExistingChildrenRef.current = React.Children.map(children, (child) =>
+            React.isValidElement(child)
+            ? React.cloneElement(child,
+                // props:
+                {
+                    key: (++childrenKeyCounter), // helps React that the last <Element> was replaced with another <Element>
+                },
+            )
+            : child
+        );
+    } // if
+    
+    
+    
+    // children:
+    return [hasChildren, lastExistingChildrenRef.current];
+}
