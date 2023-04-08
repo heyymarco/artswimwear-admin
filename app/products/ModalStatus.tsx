c61902d325a2604cf4770da5af4dade41d5b421a
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-08 23:59:40 +0700

    updated ModalStatus

--- app/products/ModalStatus.tsx
+++ app/products/ModalStatus.tsx
@@ -10,86 +10,49 @@ import {
 }                           from 'react'
 
 import {
-    GenericProps,
-    
-    
-    
-    Content,
-    
-    
-    
-    ModalProps,
-    Modal,
+    ModalCardProps,
+    ModalCard,
 }                           from '@reusable-ui/components'              // a set of official Reusable-UI components
 
 
 
 export interface ModalStatusProps
     extends
-        Omit<ModalProps,
+        Omit<ModalCardProps,
             // states:
             |'expanded' // replaced with auto expanded `children` as *status*
-            
-            
-            
-            // children:
-            |'children' // replaced with auto expanded `children` as *status*
         >
 {
-    // components:
-    contentComponent ?: React.ReactComponentElement<any, GenericProps<Element>>
-    
-    
-    
-    // children:
-    children         ?: React.ReactNode
 }
 const ModalStatus = (props: ModalStatusProps): JSX.Element|null => {
     // rest props:
     const {
-        // components:
-        contentComponent = (<Content /> as React.ReactComponentElement<any, GenericProps<Element>>),
-        
-        
-        
         // children:
-        children : status,
+        children,
     ...restModalProps} = props;
     
     
     
     // states:
-    const lastStatusRef = useRef<React.ReactNode>(status);
-    const hasStatus     : boolean = (!!status || (status === 0)) && (status !== true); // ignores undefined|null|true|false|emptyString
-    if (hasStatus) lastStatusRef.current = status;
+    const lastChildrenRef = useRef<React.ReactNode>(children);
+    const hasChildren     : boolean = (!!children || (children === 0)) && (children !== true); // ignores undefined|null|true|false|emptyString
+    if (hasChildren) lastChildrenRef.current = children;
     
     
     
     // jsx:
     return (
-        <Modal
+        <ModalCard
             // other props:
             {...restModalProps}
             
             
             
             // states:
-            expanded={hasStatus}
+            expanded={hasChildren}
         >
-            {/* <Content> */}
-            {React.cloneElement<GenericProps<Element>>(contentComponent,
-                // props:
-                {
-                    // semantics:
-                    tag : contentComponent.props.tag ?? 'article',
-                },
-                
-                
-                
-                // children:
-                lastStatusRef.current,
-            )}
-        </Modal>
+            {lastChildrenRef.current}
+        </ModalCard>
     );
 }
 export {
