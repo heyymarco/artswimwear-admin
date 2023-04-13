965b2d8051fff0f1d721628d32cb4313aac58399
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-13 20:55:56 +0700

    updated api

--- store/features/api/apiSlice.ts
+++ store/features/api/apiSlice.ts
@@ -5,12 +5,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 export interface ProductEntry {
-    _id        : string
-    visibility : string
-    name       : string
-    price      : number
-    stock      : string
-    image      : string
+    _id            : string
+    
+    visibility     : string
+    
+    name           : string
+    
+    price          : number
+    shippingWeight : number|undefined
+    
+    stock          : number|undefined
+    
+    description    : string|undefined
+    images         : string[]
+    path           : string|undefined
 }
 export interface PagedProductEntries {
     total    : number
