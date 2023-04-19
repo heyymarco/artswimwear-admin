945d7c2d45e996780d0f2335ffdf2cebefdcdce8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-04-19 23:42:20 +0700

    updated Tab

--- components/Tab/styles/styles.ts
+++ components/Tab/styles/styles.ts
@@ -4,59 +4,131 @@ import {
     rule,
     children,
     style,
+    vars,
 }                           from '@cssfn/core'                  // writes css in javascript
 
 // reusable-ui core:
 import {
     // border (stroke) stuff of UI:
     usesBorder,
+    
+    
+    
+    // padding (inner spacing) stuff of UI:
+    usesPadding,
+    
+    
+    
+    // groups a list of UIs into a single UI:
+    usesGroupable,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
+// reusable-ui components:
+import {
+    // configs:
+    basics,
+}                           from '@reusable-ui/components'      // a set of official Reusable-UI components
 
 
-export const usesTabBodyLayout = () => {
+export const usesTabLayout = () => {
     // dependencies:
     
     // features:
     const {borderVars} = usesBorder();
+    const {paddingVars} = usesPadding();
+    const {groupableRule} = usesGroupable({
+        orientationInlineSelector : null, // never  => the <TabHeader> & <TabBody> are never  stacked in horizontal
+        orientationBlockSelector  : '&',  // always => the <TabHeader> & <TabBody> are always stacked in vertical
+        itemsSelector             : ':nth-child(n)', // select <TabHeader> & <TabBody>
+    });
     
     
     
     return style({
-        // children:
-        ...children('.tabBody', {
-            // layouts:
-            display      : 'grid',
-            justifyItems : 'stretch', // overlaps each <TabOption> to anothers
-            alignItems   : 'stretch', // overlaps each <TabOption> to anothers
+        // features:
+        ...groupableRule(), // make a nicely rounded corners
+        
+        
+        
+        // layouts:
+        ...style({
+            // children:
+            ...children('.tabBody', {
+                // layouts:
+                display      : 'grid',
+                justifyItems : 'stretch', // overlaps each <TabOption> to anothers
+                alignItems   : 'stretch', // overlaps each <TabOption> to anothers
+                
+                
+                
+                // borders:
+                [borderVars.borderStartStartRadius] : '0px', // remove top radius
+                [borderVars.borderStartEndRadius  ] : '0px', // remove top radius
+                borderBlockStartWidth               : '0px', // remove top border (already applied by <Tab>)
+                
+                
+                
+                // children:
+                ...children('.tabOption', {
+                    // positions:
+                    gridArea: '1/1/1/1', // the options are overlapping each other, so the parent takes the maximum width & height of children
+                    
+                    
+                    
+                    // appearances:
+                    ...rule(':not(.expanded)', {
+                        // appearances:
+                        visibility: 'hidden', // hide inactive <TabOption>
+                    }),
+                }),
+            }),
             
             
             
             // borders:
-            [borderVars.borderStartStartRadius] : '0px', // remove top radius
-            [borderVars.borderStartEndRadius  ] : '0px', // remove top radius
-            borderBlockStartWidth               : '0px', // remove top border (already applied by <Tab>)
+         // border                 : borderVars.border, // no need border
+         // borderRadius           : borderVars.borderRadius,
+            borderStartStartRadius : borderVars.borderStartStartRadius,
+            borderStartEndRadius   : borderVars.borderStartEndRadius,
+            borderEndStartRadius   : borderVars.borderEndStartRadius,
+            borderEndEndRadius     : borderVars.borderEndEndRadius,
             
             
             
-            // children:
-            ...children('.tabOption', {
-                // positions:
-                gridArea: '1/1/1/1', // the options are overlapping each other, so the parent takes the maximum width & height of children
-                
-                
-                
-                // appearances:
-                ...rule(':not(.expanded)', {
-                    // appearances:
-                    visibility: 'hidden', // hide inactive <TabOption>
-                }),
-            }),
+            // spacings:
+            padding: 0,
+        }),
+        
+        
+        
+        // features:
+        
+        // use manually applying `borderRule` feature to avoid useless `borderVars.borderColorFn` & `borderVars.borderColor`
+     // ...borderRule(),  // must be placed at the last
+        
+        // use manually applying `paddingRule` feature to avoid useless `paddingVars.padding`
+     // ...paddingRule(), // must be placed at the last
+        
+        
+        
+        // manually applying `borderRule` feature:
+        ...vars({
+            // borders:
+            [borderVars.borderStartStartRadius] : '0px',
+            [borderVars.borderStartEndRadius  ] : '0px',
+            [borderVars.borderEndStartRadius  ] : basics.borderRadius,
+            [borderVars.borderEndEndRadius    ] : basics.borderRadius,
+            
+            
+            
+            // spacings:
+            [paddingVars.paddingInline        ] : '0px',
+            [paddingVars.paddingBlock         ] : '0px',
         }),
     });
 };
 
 export default () => style({
     // layouts:
-    ...usesTabBodyLayout(),
+    ...usesTabLayout(),
 });
