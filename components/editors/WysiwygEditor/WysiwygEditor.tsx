ceb6de305d892b65e3a4af4b69dc922d61b4f2f5
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-05 11:42:56 +0700

    modularized theme

--- components/editors/WysiwygEditor/WysiwygEditor.tsx
+++ components/editors/WysiwygEditor/WysiwygEditor.tsx
@@ -109,6 +109,12 @@ import type {
     EditorProps,
 }                           from '@/components/editors/Editor'
 
+// theme:
+import {
+    // defined classes to match Reusable-UI's styles & components.
+    theme,
+}                           from './theme'
+
 // behaviors:
 import {
     // setups the initial value for the editor.
@@ -225,92 +231,6 @@ const WysiwygEditor = <TElement extends Element = HTMLElement>(props: WysiwygEdi
     
     
     // configs:
-    const theme = useMemo<EditorThemeClasses>(() => ({
-     // ltr                        : 'ltr',
-     // rtl                        : 'rtl',
-        
-     // placeholder                : 'placeholder',
-        
-        // texts:
-        text: {
-     //     bold                   : 'b',
-     //     italic                 : 'i',
-            underline              : 'insert',
-            strikethrough          : 'delete',
-            underlineStrikethrough : 'insert delete',
-            
-            subscript              : 'subscript',   // TODO: update style
-            superscript            : 'superscript', // TODO: update style
-            
-     //     code                   : 'code',
-        },
-     // paragraph                  : 'p',
-     // heading: {
-     //     h1                     : 'h1',
-     //     h2                     : 'h2',
-     //     h3                     : 'h3',
-     //     h4                     : 'h4',
-     //     h5                     : 'h5',
-     //     h6                     : 'h6',
-     // },
-     // quote                      : 'quote',
-        
-        // resources:
-        link                       : 'link',  // TODO: update style
-        image                      : 'image', // TODO: update style
-        
-        // layouts:
-     // list: {
-     //     nested: {
-     //         listitem           : 'nested-listitem',
-     //     },
-     //     ol                     : 'list-ol',
-     //     ul                     : 'list-ul',
-     //     listitem               : 'listItem',
-     //     listitemChecked        : 'listItemChecked',
-     //     listitemUnchecked      : 'listItemUnchecked',
-     // },
-     // table                      : 'table',
-        
-        // identifiers:
-     // hashtag                    : 'hashtag',
-        
-        // codes:
-        code                       : 'block',
-     // codeHighlight: {
-     //     atrule                 : 'codeAttr',
-     //     attr                   : 'codeAttr',
-     //     boolean                : 'codeProperty',
-     //     builtin                : 'codeSelector',
-     //     cdata                  : 'codeComment',
-     //     char                   : 'codeSelector',
-     //     class                  : 'codeFunction',
-     //     'class-name'           : 'codeFunction',
-     //     comment                : 'codeComment',
-     //     constant               : 'codeProperty',
-     //     deleted                : 'codeProperty',
-     //     doctype                : 'codeComment',
-     //     entity                 : 'codeOperator',
-     //     function               : 'codeFunction',
-     //     important              : 'codeVariable',
-     //     inserted               : 'codeSelector',
-     //     keyword                : 'codeAttr',
-     //     namespace              : 'codeVariable',
-     //     number                 : 'codeProperty',
-     //     operator               : 'codeOperator',
-     //     prolog                 : 'codeComment',
-     //     property               : 'codeProperty',
-     //     punctuation            : 'codePunctuation',
-     //     regex                  : 'codeVariable',
-     //     selector               : 'codeSelector',
-     //     string                 : 'codeSelector',
-     //     symbol                 : 'codeProperty',
-     //     tag                    : 'codeProperty',
-     //     url                    : 'codeOperator',
-     //     variable               : 'codeVariable',
-     // },
-    }), []);
-    
     const initialConfig : InitialConfigType = useMemo(() => ({
         namespace   : 'WysiwygEditor', 
         theme,
