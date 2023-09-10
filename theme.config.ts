06c9b988048443c51eed9c21a2a9fb1d66bb8da2
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-09-10 08:59:49 +0700

    updated blue navy theme

--- theme.config.ts
+++ theme.config.ts
@@ -1,16 +1,42 @@
 import '@reusable-ui/typos/effects'
-import { defineTheme } from '@reusable-ui/core'
+import { defineTheme, colorValues } from '@reusable-ui/core'
 import { iconConfig } from '@reusable-ui/components'
 import { galleryEditors } from '@/components/editors/GalleryEditor/styles/config'
 import { commerces } from '@/config'
+import { basicValues } from '@reusable-ui/components'
 import { imageValues } from '@heymarco/image'
 import { carouselValues } from '@reusable-ui/components'
 
+// other libs:
+import Color                from 'color'                // color utilities
 
 
-// themes:
-defineTheme('primary', 'hsl(28, 60%, 10%)');
-defineTheme('secondary', 'hsl(28, 30%, 80%)');
+
+// light theme
+colorValues.backg = colorValues.light;
+colorValues.foreg = colorValues.dark;
+
+const primaryCol = Color('hsl(185, 35%, 60%)').darken(0.2).saturate(0.2);
+defineTheme('primary',    primaryCol);
+colorValues.primaryText = primaryCol.lighten(1);
+colorValues.primaryMild = primaryCol.lighten(1);
+// colorValues.primaryMild = primaryCol.darken(0.5);
+
+const secondaryCol = primaryCol.rotate(5).desaturate(0.5).lighten(0.65);
+defineTheme('secondary', secondaryCol);
+colorValues.secondaryText = secondaryCol.darken(0.6);
+colorValues.secondaryMild = secondaryCol.lighten(1);
+
+// colorValues.dark = primaryCol.darken(0.5);
+// colorValues.light = primaryCol.lighten(0.5);
+// colorValues.dark = primaryCol.lighten(0.5);
+// colorValues.light = primaryCol.darken(0.5);
+
+
+basicValues.backgGrad = [
+    ['linear-gradient(180deg, transparent, rgba(100, 100, 100, 1)) border-box'],
+];
+basicValues.backgroundBlendMode = 'saturation';
 
 
 
