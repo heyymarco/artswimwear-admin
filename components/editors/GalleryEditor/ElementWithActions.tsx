079ac15fd787e25ccd29688680daa42308706f60
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-09-19 10:55:39 +0700

    updated GalleryEditor

--- components/editors/GalleryEditor/ElementWithActions.tsx
+++ components/editors/GalleryEditor/ElementWithActions.tsx
@@ -126,7 +126,7 @@ const ElementWithActions = <TElement extends Element = HTMLElement>(props: Eleme
             // ignore any error
         }
         finally {
-            if (!isMounted.current) return; // the component was unloaded before awaiting returned => ignore
+            if (!isMounted.current) return; // the component was unloaded before awaiting returned => do nothing
             setIsBusy(isBusy /* instant update without waiting for (slow|delayed) re-render */ = false);
         } // try
     });
