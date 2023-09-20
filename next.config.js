6f91c67bcfcaf2e05432faaa6f7a8c3a3e9dedd6
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-09-20 13:17:01 +0700

    updated dependencies & nextjs

--- next.config.js
+++ next.config.js
@@ -11,18 +11,18 @@ const nextConfig = {
     ],
   },
   
-  experimental: {
-    appDir: true,
-    esmExternals: 'loose',
-  },
+  // experimental: {
+  //   appDir: true,
+  //   esmExternals: 'loose',
+  // },
   
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      ...{topLevelAwait: true}
-    }
-    return config
-  },
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     ...{topLevelAwait: true}
+  //   }
+  //   return config
+  // },
 }
 
 module.exports = nextConfig
