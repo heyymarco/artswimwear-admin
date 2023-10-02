9bca19afed75a5db8142ed62d7ee0a39888501c8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-10-02 07:54:59 +0700

    updated ComplexEditModelDialog

--- components/dialogs/ComplexEditModelDialog/ComplexEditModelDialogStyles.ts
+++ components/dialogs/ComplexEditModelDialog/ComplexEditModelDialogStyles.ts
@@ -4,15 +4,26 @@ import {
     style,
     vars,
     scope,
-}                           from '@cssfn/core'          // writes css in javascript
+}                           from '@cssfn/core'                  // writes css in javascript
 import {
     // border (stroke) stuff of UI:
     usesBorder,
-}                           from '@reusable-ui/core'    // a set of reusable-ui packages which are responsible for building any component
+    
+    
+    
+    // a responsive management system:
+    ifScreenWidthAtLeast,
+    
+    
+    
+    // groups a list of UIs into a single UI:
+    breakpoints,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
 // reusable-ui components:
 import {
     // configs:
+    containers,
     lists,
 }                           from '@reusable-ui/components'      // a set of official Reusable-UI components
 
@@ -31,17 +42,23 @@ export const usesCardBodyLayout  = () => {
         // layouts:
         ...style({
             // layouts:
-            display        : 'flex',
-            flexDirection  : 'column',
-            justifyContent : 'start',       // if items are not growable, the excess space (if any) placed at the end, and if no sufficient space available => the first item should be visible first
-            alignItems     : 'stretch',     // items width are 100% of the parent (for variant `.block`) or height (for variant `.inline`)
-            flexWrap       : 'nowrap',      // no wrapping
+            display           : 'flex',
+            flexDirection     : 'column',
+            justifyContent    : 'start',       // if items are not growable, the excess space (if any) placed at the end, and if no sufficient space available => the first item should be visible first
+            alignItems        : 'stretch',     // items width are 100% of the parent (for variant `.block`) or height (for variant `.inline`)
+            flexWrap          : 'nowrap',      // no wrapping
             
             
             
             // sizes:
             // the default <Card>'s body height is resizeable, ensuring footers are aligned to the bottom:
-            flex           : [[1, 1, 'auto']], // growable, shrinkable, initial from it's width (for variant `.inline`) or height (for variant `.block`)
+            flex              : [[1, 1, 'auto']], // growable, shrinkable, initial from it's width (for variant `.inline`) or height (for variant `.block`)
+            
+            boxSizing         : 'border-box',
+            minInlineSize     : `calc(100vw - (${containers.paddingInline} * 2))`,
+            ...ifScreenWidthAtLeast('md', {
+                minInlineSize : `${breakpoints.sm}px`,
+            }),
             
             
             
