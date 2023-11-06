81f988433bd854e2e2a0b479bc4155fe94a99968
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-11-07 03:20:56 +0700

    updated theme

--- theme.config.ts
+++ theme.config.ts
@@ -42,7 +42,7 @@ colorValues.secondaryMild = secondaryCol.lighten(1);
 
 // <Basic>:
 basicValues.backgGrad = [
-    ['linear-gradient(180deg, transparent, rgba(100, 100, 100, 1)) border-box'],
+    ['linear-gradient(180deg, transparent, rgba(220, 220, 220, 1)) border-box'],
 ];
 basicValues.backgroundBlendMode = 'saturation';
 
