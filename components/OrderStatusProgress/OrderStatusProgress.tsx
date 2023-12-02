015db4cb53958f575f9223141d53b5e66d4d5aa3
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-02 14:08:53 +0700

    removed unused imports

--- components/OrderStatusProgress/OrderStatusProgress.tsx
+++ components/OrderStatusProgress/OrderStatusProgress.tsx
@@ -4,74 +4,27 @@
 import {
     // react:
     default as React,
-    
-    
-    
-    // hooks:
-    useState,
 }                           from 'react'
-
-// reusable-ui core:
-import {
-    // react helper hooks:
-    useEvent,
-    useMountedFlag,
-}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
 
 // reusable-ui components:
 import {
-    // simple-components:
-    Icon,
-    ButtonIconProps,
-    ButtonIcon,
-    
-    
-    
     // layout-components:
     ListItem,
     ListProps,
     List,
-    
-    
-    
-    // menu-components:
-    DropdownListButton,
-    
-    
-    
-    // composite-components:
-    GroupProps,
-    Group,
-    
-    
-    
-    // utility-components:
-    useDialogMessage,
 }                           from '@reusable-ui/components'          // a set of official Reusable-UI components
 
 // internal components:
 import {
     orderStatusValues,
-    orderStatusTheme,
     orderStatusText,
-    orderStatusIcon,
-    orderStatusNext,
-    orderStatusTextNext,
 }                           from '@/components/OrderStatusBadge'
-import {
-    RadioDecorator,
-}                           from '@/components/RadioDecorator'
 
 // stores:
 import type {
     // types:
     OrderDetail,
 }                           from '@/store/features/api/apiSlice'
-
-// models:
-import type {
-    OrderStatus,
-}                           from '@prisma/client'
 
 
 
