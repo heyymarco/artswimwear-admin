8d1387841f765339f7c297df24ec880f7f9c4d9c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-01-28 15:15:17 +0700

    updated draggable

--- libs/drag-n-drop/styles/styles.ts
+++ libs/drag-n-drop/styles/styles.ts
@@ -5,6 +5,12 @@ import {
     children,
 }                           from '@cssfn/core'                      // writes css in javascript
 
+// reusable-ui core:
+import {
+    // a capability of UI to stack on top-most of another UI(s) regardless of DOM's stacking context:
+    globalStacks,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
 
 
 // styles:
@@ -12,7 +18,7 @@ export const usesDragOverlayLayout = () => {
     return style({
         // positions:
         position : 'fixed',
-        zIndex   : 999999, // TODO: update with global-stackable
+        zIndex   : globalStacks.dragOverlay,
         
         
         
