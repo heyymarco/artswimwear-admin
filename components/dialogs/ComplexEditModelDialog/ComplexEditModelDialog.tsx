7b75a86405a2e8a7b0dd864a635de3bc4e23f71c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-03-02 10:06:18 +0700

    added UpdateDraftHandler

--- components/dialogs/ComplexEditModelDialog/ComplexEditModelDialog.tsx
+++ components/dialogs/ComplexEditModelDialog/ComplexEditModelDialog.tsx
@@ -95,6 +95,7 @@ export type ComplexEditModelDialogResult<TModel extends Model> = PartialModel<TM
 export interface ComplexEditModelDialogExpandedChangeEvent<TModel extends Model> extends ModalExpandedChangeEvent<ComplexEditModelDialogResult<TModel>> {}
 
 export type UpdateHandler<TModel extends Model>         = (args: { id: string|null, whenAdd: boolean, whenUpdate: Record<string, boolean> }) => PartialModel<TModel>|Promise<PartialModel<TModel>>
+export type UpdateDraftHandler<TModel extends Model>    = (args: { draftModel: TModel, whenAdd: boolean, whenUpdate: Record<string, boolean> }) => PartialModel<TModel>|Promise<PartialModel<TModel>>
 export type UpdatedHandler<TModel extends Model>        = (updatedModel: PartialModel<TModel>) => void|Promise<void>
 export type AfterUpdateHandler                          = () => void|Promise<void>
 
