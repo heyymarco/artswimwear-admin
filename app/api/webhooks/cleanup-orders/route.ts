c2845ee4238e98be9c680de216a57f3ee5d35728
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-01 19:58:17 +0700

    updated cleanup-orders webhook

--- app/api/webhooks/cleanup-orders/route.ts
+++ app/api/webhooks/cleanup-orders/route.ts
@@ -1,9 +1,9 @@
 export async function POST(req: Request, res: Response): Promise<Response> {
     const secretHeader = req.headers.get('X-Secret');
-    console.log('webhook called: ', {secretHeader});
+    console.log('webhook:cleanup-orders called: ', {secretHeader});
     if (!secretHeader || (secretHeader !== process.env.APP_SECRET)) {
         return Response.json({
-            error: 'Invalid data.',
+            error: 'Unauthorized.',
         }, { status: 401 }); // handled with error: unauthorized
     } // if
     
