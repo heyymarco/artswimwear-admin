a2d43f7ff7f295a789c80f0910381206e88135e9
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-08 05:12:56 +0700

    disabled cronjob

--- app/api/(private)/webhooks/cleanup-orders/route.ts
+++ app/api/(private)/webhooks/cleanup-orders/route.ts
@@ -34,10 +34,10 @@ export const maxDuration = 60; // this function can run for a maximum of 60 seco
 
 
 export async function POST(req: Request, res: Response): Promise<Response> {
-    // console.log('ignore cronjob');
-    // return Response.json({
-    //     ok: true,
-    // });
+    console.log('ignore cronjob');
+    return Response.json({
+        ok: true,
+    });
     
     
     
