3faafad65dc5f7daeba765149512f6bc7bce4f60
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-14 20:59:13 +0700

    added CountryDisplay

--- components/editors/SelectCountryEditor/index.ts
+++ components/editors/SelectCountryEditor/index.ts
@@ -1,2 +1,3 @@
 export * from './SelectCountryEditor'
+export * from './CountryDisplay'
 export * from './utilities'
--- components/editors/SelectCountryEditor/utilities.ts
+++ components/editors/SelectCountryEditor/utilities.ts
@@ -5,6 +5,6 @@ import {
 
 
 
-export const getCountryByCode  = (countryCode: string|null): string|null => (countryCode ? defaultCountryMap.get(countryCode) : null) ?? null;
-export const getCountryDisplay = (countryCode: string|null): string => getCountryByCode(countryCode) ?? 'Select Country';
+export const getCountryByCode  = (countryCode: string|null|undefined): string|null => (countryCode ? defaultCountryMap.get(countryCode) : null) ?? null;
+export const getCountryDisplay = (countryCode: string|null|undefined): string => getCountryByCode(countryCode) ?? 'Select Country';
 export const countryList       = Array.from(defaultCountryMap.keys());
