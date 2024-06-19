852d9abb3ba8ecdb231582e1ddf5e3cea93b865c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-20 01:48:29 +0700

    updated SelectZoneEditor

--- components/editors/SelectZoneEditor.tsx
+++ components/editors/SelectZoneEditor.tsx
@@ -42,6 +42,7 @@ const SelectZoneEditor = <TElement extends Element = HTMLButtonElement>(props: S
         
         // accessibilities:
         'aria-label' : ariaLabel = `Select ${startsCapitalized(modelName)}`,
+        placeholder  = ariaLabel,
         
         
         
@@ -66,6 +67,7 @@ const SelectZoneEditor = <TElement extends Element = HTMLButtonElement>(props: S
             
             // accessibilities:
             aria-label={ariaLabel}
+            placeholder={placeholder}
             
             
             
