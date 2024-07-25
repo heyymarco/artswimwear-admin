ff3a5df01a1c809bff89d696f6430bbc3e5a5882
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-26 05:06:03 +0700

    updated images lib

--- libs/images.ts
+++ libs/images.ts
@@ -1,9 +1,9 @@
 export const downloadImageAsBase64 = async (url: string, responsiveSize: number, quality: number = 75): Promise<string> => {
     const response    = await fetch(`${process.env.APP_URL ?? ''}/_next/image?url=${encodeURIComponent(url)}&w=${responsiveSize}&q=${quality}`, {
         cache : 'force-cache',
-        next  : {
-            revalidate : 1 * 24 * 3600, // set the cache lifetime of a resource (in seconds).
-        },
+        // next  : { // "cache: force-cache" and "revalidate: 86400", only one should be specified
+        //     revalidate : 1 * 24 * 3600, // set the cache lifetime of a resource (in seconds).
+        // },
     });
     if (response.status !== 200) throw Error('Unable to download the image.');
     const blob        = await response.blob();
