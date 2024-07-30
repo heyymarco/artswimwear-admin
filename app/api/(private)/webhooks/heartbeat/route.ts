bebd12ba73fc8301fa640a0448d20db76d618e5c
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-07-31 02:18:48 +0700

    updated heartbeat webhook

--- app/api/(private)/webhooks/heartbeat/route.ts
+++ app/api/(private)/webhooks/heartbeat/route.ts
@@ -34,15 +34,7 @@ export const maxDuration = 60; // this function can run for a maximum of 60 seco
 
 
 export async function POST(req: Request, res: Response): Promise<Response> {
-    // console.log('ignore cronjob');
-    // return Response.json({
-    //     ok: true,
-    // });
-    
-    
-    
     const secretHeader = req.headers.get('X-Secret');
-    console.log('webhook:cleanup-orders called: ', {secretHeader});
     if (!secretHeader || (secretHeader !== process.env.APP_SECRET)) {
         return Response.json({
             error: 'Unauthorized.',
@@ -126,7 +118,6 @@ export async function POST(req: Request, res: Response): Promise<Response> {
     
     
     // Return OK:
-    console.log('heartbeat');
     return Response.json({
         ok: true,
     });
