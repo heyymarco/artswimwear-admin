da884e3251206c6b2396ecf23a886bf4106ca98e
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-06 22:22:32 +0700

    updated models

--- models/index.ts
+++ models/index.ts
@@ -1,12 +1,18 @@
 export      * from './utilities'
 
 export type * from '@prisma/client'
+
+export      * from './commons'
+
 export      * from './products'
-export      * from './customers'
-export      * from './admins'
-export      * from './orders'
 export      * from './shippings'
-export      * from './emails'
-export      * from './roles'
+
+export      * from './orders'
+
+export      * from './customers'
 export      * from './shipment'
 export      * from './shipping-labels'
+export      * from './emails'
+
+export      * from './admins'
+export      * from './roles'
