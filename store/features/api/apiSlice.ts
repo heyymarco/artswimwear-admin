d07cc806f21d0aa1ba5d8756e070fa293644bb17
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-14 21:02:56 +0700

    updated apiSlice

--- store/features/api/apiSlice.ts
+++ store/features/api/apiSlice.ts
@@ -928,7 +928,7 @@ const cumulativeUpdateEntityCache     = async <TEntry extends Model|string, TQue
         // update cache:
         for (const { originalArgs } of updatedCollectionQueryCaches) {
             api.dispatch(
-                apiSlice.util.updateQueryData(endpointName, undefined, (data) => {
+                apiSlice.util.updateQueryData(endpointName, originalArgs as any, (data) => {
                     (data.entities as Dictionary<TEntry>)[mutatedId] = mutatedEntry; // replace oldEntry with mutatedEntry
                 })
             );
@@ -952,7 +952,7 @@ const cumulativeUpdateEntityCache     = async <TEntry extends Model|string, TQue
         for (const { originalArgs } of shiftedCollectionQueryCaches) {
             // reconstruct current entity cache:
             api.dispatch(
-                apiSlice.util.updateQueryData(endpointName, undefined, (data) => {
+                apiSlice.util.updateQueryData(endpointName, originalArgs as any, (data) => {
                     // INSERT the new entry:
                     (data.entities as Dictionary<TEntry>) = {
                         [mutatedId] : mutatedEntry, // place the inserted entry to the first property
@@ -990,7 +990,7 @@ const cumulativeUpdateEntityCache     = async <TEntry extends Model|string, TQue
         for (const { originalArgs } of shiftedCollectionQueryCaches) {
             // reconstruct current entity cache:
             api.dispatch(
-                apiSlice.util.updateQueryData(endpointName, undefined, (data) => {
+                apiSlice.util.updateQueryData(endpointName, originalArgs as any, (data) => {
                     // REMOVE the deleted entry:
                     delete (data.entities as Dictionary<TEntry>)[mutatedId];
                     
