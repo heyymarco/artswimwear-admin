16b0cb05e2549a70bfaae7cabaf6ec02acb5ad76
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2025-01-30 22:26:03 +0700

    refactor: update ShippingWeightEditor to improve type safety and prop handling

--- components/editors/ShippingWeightEditor.tsx
+++ components/editors/ShippingWeightEditor.tsx
@@ -11,6 +11,11 @@ import {
 
 // reusable-ui core:
 import {
+    // a collection of TypeScript type utilities, assertions, and validations for ensuring type safety in reusable UI components:
+    type NoForeignProps,
+    
+    
+    
     // react helper hooks:
     useMergeStyles,
 }                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
@@ -19,8 +24,6 @@ import {
 import {
     // react components:
     Label,
-    
-    Input,
 }                           from '@reusable-ui/components'      // a set of official Reusable-UI components
 
 // heymarco components:
@@ -38,18 +41,23 @@ import {
 
 
 // react components:
-export interface ShippingWeightEditorProps<TElement extends Element = HTMLSpanElement>
+export interface ShippingWeightEditorProps<out TElement extends Element = HTMLDivElement, TValue extends number|null = number|null, in TChangeEvent extends React.SyntheticEvent<unknown, Event> = React.ChangeEvent<HTMLInputElement>>
     extends
         // bases:
-        NumberUpDownEditorProps<TElement>
+        NumberUpDownEditorProps<TElement, TValue, TChangeEvent>
 {
 }
-const ShippingWeightEditor = <TElement extends Element = HTMLSpanElement>(props: ShippingWeightEditorProps<TElement>): JSX.Element|null => {
+const ShippingWeightEditor = <TElement extends Element = HTMLDivElement, TValue extends number|null = number|null, TChangeEvent extends React.SyntheticEvent<unknown, Event> = React.ChangeEvent<HTMLInputElement>>(props: ShippingWeightEditorProps<TElement, TValue, TChangeEvent>): JSX.Element|null => {
     // rest props:
     const {
         // components:
-        numberEditorComponent = (<NumberEditor<TElement, number|null, React.ChangeEvent<HTMLInputElement>> /> as React.ReactComponentElement<any, NumberEditorProps<TElement, number|null, React.ChangeEvent<HTMLInputElement>>>),
-    ...restNumberUpDownEditorProps} = props;
+        numberEditorComponent = (<NumberEditor<Element, TValue, TChangeEvent> /> as React.ReactElement<NumberEditorProps<Element, TValue, TChangeEvent>>),
+        
+        
+        
+        // other props:
+        ...restShippingWeightEditorProps
+    } = props;
     
     
     
@@ -69,29 +77,54 @@ const ShippingWeightEditor = <TElement extends Element = HTMLSpanElement>(props:
     
     
     
+    // default props:
+    const {
+        // accessibilities:
+        'aria-label' : ariaLabel = 'Shipping Weight',
+        
+        
+        
+        // validations:
+        required                 = false,
+        min                      = 0,
+        max                      = 99,
+        step                     = 0.01,
+        
+        
+        
+        // children:
+        childrenAfterInput       = <Label className='solid'>Kg</Label>,
+        
+        
+        // other props:
+        ...restNumberUpDownEditorProps
+    } = restShippingWeightEditorProps satisfies NoForeignProps<typeof restShippingWeightEditorProps, NumberUpDownEditorProps<TElement, TValue, TChangeEvent>>;
+    
+    
+    
     // jsx:
     return (
-        <NumberUpDownEditor<TElement>
+        <NumberUpDownEditor<TElement, TValue, TChangeEvent>
             // other props:
             {...restNumberUpDownEditorProps}
             
             
             
             // accessibilities:
-            aria-label={props['aria-label'] ?? 'Shipping Weight'}
+            aria-label={ariaLabel}
             
             
             
             // validations:
-            required={props.required ?? false}
-            min={props.min ?? 0}
-            max={props.max ?? 99}
-            step={props.step ?? 0.01}
+            required={required}
+            min={min}
+            max={max}
+            step={step}
             
             
             
             // components:
-            numberEditorComponent={React.cloneElement<NumberEditorProps<TElement, number|null, React.ChangeEvent<HTMLInputElement>>>(numberEditorComponent,
+            numberEditorComponent={React.cloneElement<NumberEditorProps<Element, TValue, TChangeEvent>>(numberEditorComponent,
                 // props:
                 {
                     // styles:
@@ -102,11 +135,11 @@ const ShippingWeightEditor = <TElement extends Element = HTMLSpanElement>(props:
             
             
             // children:
-            childrenAfterInput={props.childrenAfterInput ?? <Label className='solid'>Kg</Label>}
+            childrenAfterInput={childrenAfterInput}
         />
     );
 };
 export {
-    ShippingWeightEditor,
-    ShippingWeightEditor as default,
+    ShippingWeightEditor,            // named export for readibility
+    ShippingWeightEditor as default, // default export to support React.lazy
 }
